d670897ddd17488326169131212027e890adc858
dougwollison <dougwollison@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-03 23:47:53 +0000

    Tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/nlingual/tags/1.0.1@782269 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/admin/misc.php
@@ -0,0 +1,53 @@
+<?php
+// ===================== //
+//	Miscellaneous Hooks  //
+// ===================== //
+
+/*
+ * Enqueue admin styles/scripts
+ */
+add_action('admin_enqueue_scripts', 'nLingual_enqueue_scripts');
+function nLingual_enqueue_scripts(){
+	// Admin styling
+	wp_register_style('nLingual-admin', plugins_url('css/admin.css', NL_SELF), '1.0', 'screen');
+
+	// Admin javascript
+	wp_register_script('nLingual-admin-js', plugins_url('js/admin.js', NL_SELF), array('jquery-ui-sortable'), '1.0');
+	wp_localize_script('nLingual-admin-js', 'nLingual_l10n', array(
+		'NoPostSelected' => __('No post selected to edit.', NL_TXTDMN),
+		'EraseDataConfirm' => __('Are you sure you wish to erase all language and translation data?', NL_TXTDMN)
+	));
+
+	// Quick-Edit javascript
+	wp_register_script('nLingual-quickedit-js', plugins_url('js/quickedit.js', NL_SELF), array('inline-edit-post'), '1.0', true);
+
+	wp_enqueue_style('nLingual-admin');
+	wp_enqueue_script('nLingual-admin-js');
+	wp_enqueue_script('nLingual-quickedit-js');
+}
+
+/*
+ * Add link to settings page from plugin entry on Plugins page
+ */
+add_filter('plugin_action_links', 'nLingual_plugin_links', 10, 2);
+function nLingual_plugin_links($links, $file){
+	global $this_plugin;
+	if(!$this_plugin) $this_plugin = plugin_basename(NL_SELF);
+
+	if($file == $this_plugin){
+		$settings_link = '<a href="options-general.php?page=nLingual">'.__('Settings', NL_TXTDMN).'</a>';
+		array_unshift($links, $settings_link);
+	}
+
+	return $links;
+}
+
+/*
+ * Admin Notices for any specific actions
+ */
+add_action('admin_notices', 'nLingual_admin_notices');
+function nLingual_admin_notices(){
+	if(isset($_GET['nLingual-erase']) && $_GET['nLingual-erase']){
+		printf('<div class="updated"><p>%s</p></div>', __('The translation table has been successfully erased.', NL_TXTDMN));
+	}
+}
