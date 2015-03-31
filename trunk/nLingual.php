2d9ae95d0b0e256f0e9b8523f90c36f8ec5aec6f
dougwollison <dougwollison@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 16:03:33 +0000

    Version 1.2.4 release, pulled from GitHub.
    git-svn-id: https://plugins.svn.wordpress.org/nlingual/tags/1.2.4@1124582 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/nLingual.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: nLingual
+Plugin URI: https://github.com/dougwollison/nLingual
+Description: Easy to manage Multilingual system, with theme development utilities and post data synchronization.
+Version: 1.2.4
+Author: Doug Wollison
+Author URI: http://dougw.me
+Tags: multilingual, multi, language, admin, bilingual, switcher, translation, nlingual
+License: GPL2
+*/
+
+define( 'NL_SELF', __FILE__ );
+define( 'NL_TXTDMN', 'nLingual' );
+
+require( __DIR__ . '/php/load.php' );
